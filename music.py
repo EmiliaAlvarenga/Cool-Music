b402e62fe47b11db58236d53575641e340d7932f
EmiliaAlvarenga <emilia.alvarenga@outlook.es>
2014-04-12 19:46:45 +0200

    Tratando de usar Get y Post Bottle.
    Anda que no queda nada ...

--- music.py
+++ music.py
@@ -3,24 +3,36 @@
 
 import json
 import requests
-fichero=open ("fichero.txt","w")
+from bottle import route, get, post, run, template, debug, error
+import bottle 
 
-cantante = raw_input('Search artist:')
-#url='http://api.deezer.com/search?'
-#params={q:'%s'%cantante}
-#identificador_artista=[]
+@error(404)
+def error404(error):
+    return 'Nada que mostrar aqui'
+	
+@get('/search')
+def search():
+    return '''
+        <form action="/search" method="post">
+            Artista: <input name="artista" type="text" />
+            <input value="Search" type="submit" />
+        </form>
+    '''
+	
+@post('/search')
+def nombre():
+	global artista
+	title=[]
+	artista = request.forms.get('artista')
+	respuesta=requests.get('http://api.deezer.com/search?q=%s'% artista)
+	datos=respuesta.text.encode('utf-8')
+	dicc_api=json.loads(datos)
+	raiz=dicc_api["data"]
+	for i in raiz:
+		tlista=i["title"]
+		title.append(tlista)
+		return template('index', artista=artista, title=title)
 
+ 
+run(host='localhost', port=8080, debug=True, reloader=True)
 
-respuesta=requests.get('http://api.deezer.com/search?q=%s'% cantante)
-datos=respuesta.text.encode('utf-8')
-dicc_api=json.loads(datos)
-raiz=dicc_api["data"]
-for i in raiz:
-    titulo=i["title"]
-    id_album=i["album"]["id"]  
-    enlace=i["link"]
-    print titulo
-    print id_album
-    print enlace  
-    fichero.write(titulo)
-    fichero.write(enlace)
