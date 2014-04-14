31b05fa8ea8374c0b3c3f71a7525211aab40237d
EmiliaAlvarenga <emilia.alvarenga@outlook.es>
2014-04-14 02:39:50 +0200

    Update music.py bottle framework
    Bottle experimentando

--- music.py
+++ music.py
@@ -20,27 +20,24 @@ def search():
 #Pedimos desde template el nombre del artista para usarlo como parametro el la peticion	
 @route('/artist')
 def search_name():
-	return template('artist')   
+    return template('artist')   
     
 #¿como pasar los datos al template?
 		
 @post('/info')
 def nombre():
-	global artist
-	artista = request.forms.get("artista")
-	respuesta = requests.get('http://api.deezer.com/search?q=%s'% artista)
-	datos=respuesta.text.encode('utf-8')
-	dicc_api=json.loads(datos)
-	raiz=dicc_api["data"]
-	title=[]
-	for i in raiz:
-		tlista=i["title"]
-		title.append(tlista)
-		return template('results', title=title)
+    global artist
+    artista = request.forms.get("artista")
+    respuesta = requests.get('http://api.deezer.com/search?q=%s'% artista)
+    datos=respuesta.text.encode('utf-8')
+    dicc_api=json.loads(datos)
+    raiz=dicc_api["data"]
+    title=[]
+    for i in raiz:
+        tlista=i["title"]
+	title.append(tlista)
+        return template('results', title=title)
 
 debug(True)
 run(host='localhost', port=8080, reloader=True)
-
- 
-run(host='localhost', port=8080, debug=True, reloader=True)
 
