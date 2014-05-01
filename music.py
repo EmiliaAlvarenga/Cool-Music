7948cb5ab0cf607693331e05571559c79975071d
unknown <emilia.alvarenga@outlook.es>
2014-05-01 20:41:39 +0200

    Update Usig MINGW32

--- music.py
+++ music.py
@@ -6,31 +6,21 @@ import requests
 from bottle import route, get, post, run, template, debug, error, request, static_file
 import bottle 
 
-#
-
-#por si escribo mal una direccion
-@error(404)
-def error404(error):
-    return 'Nada que mostrar aqui'
-    
-    
-#Ruta de la hoja de estilo
 @route('/css/:filename#.*#')
 def server_static(filename):
     return static_file(filename, root='./css/')	
 
-    
+@error(404)
+def error404(error):
+    return 'Nada que mostrar aqui'
+	
 @route('/home')
 def search():
     return template('index')
-
-    
-#Pedimos desde template el nombre del artista para usarlo como parametro el la peticion	
-@route('/artist')
+	
+@get('/artist')
 def search_name():
-    return template('artist')   
-    
-#¿como pasar los datos al template?
+    return template('artist')
 		
 @post('/info')
 def nombre():
@@ -44,4 +34,6 @@ def nombre():
 
 debug(True)
 run(host='localhost', port=8080, reloader=True)
+
+
 
